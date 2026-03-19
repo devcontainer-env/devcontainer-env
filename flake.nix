b57a53c1c1ea5578f8e63f737c2f42085217f0ba
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-19 17:04:12 +0400

    feat: add nix flake and devcontainer configuration
    Add Nix flake configuration with development shell providing Rust tooling,
    and devcontainer setup with Docker-in-Docker and Nix support for local
    development environments.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "devcontainer-env - direnv that bridges devcontainers and the host environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            clippy
+            rustc
+            rustfmt
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
