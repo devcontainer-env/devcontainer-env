fe799f04611acb808ec243d53c1e0ddebcc1c294
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-04-08 12:00:03 +0300

    refactor: flatten workspace structure to single package
    Move source files from crates/devcontainer-env/ to root src/ and tests/
    directories. Convert workspace Cargo.toml to package manifest. Update flake.nix
    to build from root and remove nested devcontainer-env flake configuration.
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -12,8 +12,23 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
       in
       {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          inherit (manifest) version;
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+          doCheck = false;
+          meta = with pkgs.lib; {
+            description = "direnv that bridges devcontainers and the host environment";
+            homepage = "https://github.com/devcontainer-env/devcontainer-env";
+            license = licenses.mit;
+            mainProgram = manifest.name;
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           name = "devcontainer-env";
           packages = with pkgs; [
