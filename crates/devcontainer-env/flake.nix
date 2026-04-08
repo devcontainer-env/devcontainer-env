917dd654ab6004ee6368b5368336acf276e7274f
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-04-08 11:23:50 +0300

    refactor: reorganize nix build structure and fix cargo lock issue
    - Create crates/devcontainer-env/flake.nix with complete package derivation
      - Fixes src path to point to workspace root (../../) instead of crate dir
      - Includes cargoBuildFlags to scope build to this package
      - Adds doCheck = false to skip integration tests in nix build
      - Consolidates logic from default.nix into flake configuration
    
    - Update root flake.nix to focus on devshell only
      - Removes packages.default, which now lives in crate flake
      - Simplifies root flake structure
    
    - Remove crates/devcontainer-env/default.nix (logic merged into flake.nix)
    
    This fixes the "Missing Cargo.lock from src" error when running nix build,
    which occurred because buildRustPackage requires Cargo.lock to be inside src.

---
+++ crates/devcontainer-env/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "devcontainer-env package";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          inherit (manifest) version;
+          cargoLock.lockFile = ../../Cargo.lock;
+          src = pkgs.lib.cleanSource ../..;
+          cargoBuildFlags = [
+            "--package"
+            manifest.name
+          ];
+          doCheck = false;
+        };
+      }
+    );
+}
